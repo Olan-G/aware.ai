eb02cd1655ea3ed45bf8333210f9abcdcfe2a655
gavin-cii <91460319+gavin-cii@users.noreply.github.com>
2024-06-06 17:59:40 +0800

    Update tts_code.py

--- tts_code.py
+++ tts_code.py
@@ -11,14 +11,4 @@ pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-tiny-en+pl.model')
 pipe.generate_to_notebook("""
 Hallo! Your picture seems to be someone holding a cup of coffee in front of a laptop.
 """)
-import torch
-import torch.nn.functional as F
 
-from IPython.display import Markdown, HTML
-from whisperspeech.pipeline import Pipeline
-
-pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-tiny-en+pl.model')
-
-pipe.generate_to_notebook("""
-Hallo! Your picture seems to be someone holding a cup of coffee in front of a laptop.
-""")
